51174e724a3773026019f8b83a53516810670a6f
島和之 <shima@mac2019a.local>
2020-08-08 00:43:38 +0900

    add gauche

---
+++ scheme/gauche/interpret.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dir=${0%/*}
+gosh $dir/aid-interpret.scm $*
---
+++ scheme/hat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Usage: ./hat.sh [-e func] source.sch
+# source.sch: ソースファイル名
+# func: 最初に呼び出す関数
+# Example: ./hat.sh sample/hello.sch
+dir=${0%/*}
+$dir/gauche/interpret.sh $dir/src/hat.scm $*
