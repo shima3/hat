bc41314bc26aa3a28de2a10a4e0528c19f126204
島和之 <shima@mac2019a.local>
2020-08-15 05:28:25 +0900

    Sat Aug 15 05:28:25 JST 2020

---
+++ scheme/gauche/hat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Usage: ./hat.sh [-e func] source.sch
+# source.sch: ソースファイル名
+# func: 最初に呼び出す関数
+# Example: ./hat.sh sample/hello.sch
+dir=${0%/*}
+# $dir/gauche/interpret.sh $dir/src/hat.scm $*
+$dir/interpret.sh $dir/../src/hat.scm $*
